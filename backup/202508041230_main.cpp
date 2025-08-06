0c00c553a47ce0d711c642dd7fb2f7d181241221
susumu2019 <zeatec.hirai@gmail.com>
2025-08-06 10:16:58 +0900

    202508061016

--- backup/202508041230_main.cpp
+++ backup/202508041230_main.cpp
@@ -73,8 +73,8 @@ void ARDUINO_ISR_ATTR onpublicTimer() {
 }
 
 // サーボ関係
-const int servo_min_us = 600;
-const int servo_max_us = 2400;
+const int servo_min_us = 1000;
+const int servo_max_us = 2000;
 int angleToDuty(int angle, int min_us, int max_us, int resolution, int freq) {
   int duty_max = (1 << resolution) - 1;
   float period_us = 1000000.0 / freq;
@@ -114,33 +114,20 @@ void setup() {
   timerAlarmEnable(publicTimer);
 
   // PWM設定
-  //ledcSetup(1, FREQUENCY, RESOLUTION);
+  ledcSetup(1, FREQUENCY, RESOLUTION);
   ledcSetup(2, FREQUENCY, RESOLUTION);
-  //ledcSetup(3, FREQUENCY, RESOLUTION);
-  //ledcSetup(4, FREQUENCY, RESOLUTION);
-  //ledcAttachPin(PWM1_PIN, 1);//ピン番号とPWMチャンネルの割り当て
+  ledcSetup(3, FREQUENCY, RESOLUTION);
+  ledcSetup(4, FREQUENCY, RESOLUTION);
+  ledcAttachPin(PWM1_PIN, 1);//ピン番号とPWMチャンネルの割り当て
   ledcAttachPin(PWM2_PIN, 2);
-  //ledcAttachPin(PWM3_PIN, 3);
-  //ledcAttachPin(PWM4_PIN, 4);
+  ledcAttachPin(PWM3_PIN, 3);
+  ledcAttachPin(PWM4_PIN, 4);
 
-  //delay(2000);
+  delay(2000);
 
   // LED初期点灯色
-  leds[0] = CRGB(255, 255, 255);//RGB CRGB::White;
-  // WS2812 テープを黄色点灯
-  for (int i = 0; i < WS2812_NUM; i++) {
-    tape_leds[i] = CRGB(255, 255, 255);//RGB
-  }
+  leds[0] = CRGB::White;
   FastLED.show();
-  delay(1000);
-
-  leds[0] = CRGB(0, 255, 0);//RGB CRGB::White;
-  // WS2812 テープを黄色点灯
-  for (int i = 0; i < WS2812_NUM; i++) {
-    tape_leds[i] = CRGB(0, 255, 0);//RGB
-  }
-  FastLED.show();
-  delay(1000);
 
   // delay(2000);
   // USBSerial.println("Start");
@@ -150,33 +137,26 @@ void loop() {
   M5.update();
 
   if (digitalRead(onBordSW_PIN) == 1) {
-    //setServoAngle(1, adcToDegree(analogRead(Analog1_PIN)));
+    setServoAngle(1, adcToDegree(analogRead(Analog1_PIN)));
     setServoAngle(2, adcToDegree(analogRead(Analog2_PIN)));
-    //setServoAngle(3, adcToDegree(analogRead(Analog3_PIN)));
-    //setServoAngle(4, adcToDegree(analogRead(Analog4_PIN)));
-
-    //setServoAngle(1, 90);
-    //setServoAngle(2, 90);
-    //setServoAngle(3, 90);
-    //setServoAngle(4, 90);
-
-    leds[0] = CRGB(0, 100, 0);
+    setServoAngle(3, adcToDegree(analogRead(Analog3_PIN)));
+    setServoAngle(4, adcToDegree(analogRead(Analog4_PIN)));
+    leds[0] = CRGB(100, 100, 0);
 
     // WS2812 テープを黄色点灯
     for (int i = 0; i < WS2812_NUM; i++) {
-      tape_leds[i] = CRGB(255, 255, 255);//RGB
+      tape_leds[i] = CRGB(100, 100, 0);
     }
   } else {
-    // setServoAngle(1, 90);
-    // setServoAngle(2, 90);
-    // setServoAngle(3, 90);
-    // setServoAngle(4, 90);
-
-    leds[0] = CRGB(0, 0, 255);
+    setServoAngle(1, 90);
+    setServoAngle(2, 90);
+    setServoAngle(3, 90);
+    setServoAngle(4, 90);
+    leds[0] = CRGB(0, 0, 100);
 
     // WS2812 テープを青色点灯
     for (int i = 0; i < WS2812_NUM; i++) {
-      tape_leds[i] = CRGB(0, 0, 255);
+      tape_leds[i] = CRGB(0, 0, 100);
     }
   }
 
@@ -185,6 +165,8 @@ void loop() {
     // USBSerial.printf("%04d,",adcToDegree(analogRead(Analog2_PIN)));
     // USBSerial.printf("%04d,",adcToDegree(analogRead(Analog3_PIN)));
     // USBSerial.printf("%04d",adcToDegree(analogRead(Analog4_PIN)));
+
+    USBSerial.println();
   }
 
   FastLED.show();
@@ -192,3 +174,50 @@ void loop() {
 
 
 
+// /*
+//   M5StampS3 シリアル出力安定版
+//   2025-08-03 修正版
+// */
+
+// #include <M5Unified.h>
+// #include <FastLED.h>
+// #include <ESP32Servo.h>
+// #include <Wire.h>
+
+// #define PIN_onBordLED 21 // オンボードフルカラーLEDの使用端子
+// #define PIN_onBordSW 0 // オンボードスイッチの使用端子
+
+// //LED関連
+// const int num_leds = 1;
+// CRGB leds[num_leds];
+
+// void setup() {
+//   auto cfg = M5.config();
+//   M5.begin(cfg);
+//   USBSerial.begin(115200);
+
+//   pinMode(PIN_onBordSW, INPUT);
+
+//   //オンボードLED
+//   FastLED.addLeds<WS2812B, PIN_onBordLED, GRB>(leds, num_leds);
+
+//   //LED初期点灯色指定
+//   leds[0] = CRGB(255, 255, 255);   // （赤, 緑, 青）※3色それぞれの明るさを0-255で指定
+//   FastLED.show();
+
+// }
+
+// void loop() {
+//   M5.update();
+
+//   leds[0] = CRGB(100, 100, 0);   // （赤, 緑, 青）※3色それぞれの明るさを0-255で指定
+//   FastLED.show();
+
+//   //オンボードスイッチ処理
+//   if(digitalRead(PIN_onBordSW) == HIGH){
+//     USBSerial.printf("Test Hi\n");
+//   }else{
+//     USBSerial.printf("Test Lo\n");
+//   }
+
+// }
